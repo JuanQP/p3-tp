57cd657b8cbe10b6ebcf2eb06e5d08bcac38c32c
QUINTEROS PARADA, Juan Ignacio <JIQuinteros@mpf.gov.ar>
2021-02-13 19:29:03 -0300

    Cambios de Ignacio
    Implementacion de DFS

--- src/grafos/GrafoDinamic.java
+++ src/grafos/GrafoDinamic.java
@@ -1,5 +1,6 @@
 package grafos;
 
+import java.util.Vector;
 
 public class GrafoDinamic implements GrafosTDA {
 	NodoGrafo nodo;
@@ -9,37 +10,36 @@ public class GrafoDinamic implements GrafosTDA {
 		nodo = null;
 		cantidad = 0;
 	}
+
 	public void eliminarVertice(int v) {
 		NodoGrafo aux = encontrarNodo(v);
-		
-		if(aux != null) {
-			aux = nodo; 
-			if(aux.valor == v) {
+
+		if (aux != null) {
+			aux = nodo;
+			if (aux.valor == v) {
 				nodo = aux.sig;
 				System.out.println("1");
 			}
-			while(aux != null) {
-				this.eliminarArista(aux.valor, v);	
-				if(aux.sig == this.encontrarNodo(v) && aux.sig != null) {
+			while (aux != null) {
+				this.eliminarArista(aux.valor, v);
+				if (aux.sig == this.encontrarNodo(v) && aux.sig != null) {
 					aux.sig = aux.sig.sig;
 					cantidad--;
 					System.out.println("1");
 				}
-					aux = aux.sig;
-					System.out.println("2");
+				aux = aux.sig;
+				System.out.println("2");
 			}
-			
-		}else {
+
+		} else {
 			System.out.println("El vertice no existe");
 		}
 
-		
 	}
 
 	public void agregarVertice(int v) {
-		if(this.encontrarNodo(v) == null) {
-			
-		
+		if (this.encontrarNodo(v) == null) {
+
 			NodoGrafo aux = new NodoGrafo();
 			aux.valor = v;
 			aux.lista = null;
@@ -48,62 +48,61 @@ public class GrafoDinamic implements GrafosTDA {
 			aux.marcado = false;
 			nodo = aux;
 			cantidad++;
-		}	
-			
+		}
+
 	}
 
-	
-	public int[] vertices() {
+	public int[] vertices() {			// Metodo implementado por nosotros.
 		
-		return null;
+		Vector <NodoGrafo> solucion = new Vector <NodoGrafo> ();
+		NodoGrafo n = nodo;
+		while(n != null) {
+			solucion.addElement(n);
+			n = n.sig;
+		}
+		return solucion.stream().mapToInt(nodo -> nodo.valor).toArray();
 	}
 
 	public void agregarArista(int v1, int v2, int peso) {
-		if(this.encontrarNodo(v1) != null && this.encontrarNodo(v2) != null) {
+		if (this.encontrarNodo(v1) != null && this.encontrarNodo(v2) != null) {
 			NodoArista aux = new NodoArista();
 			aux.origen = v1;
 			aux.peso = peso;
 			aux.Visitado = false;
 			aux.apunta = this.encontrarNodo(v2);
 			aux.sig = encontrarNodo(v1).lista;
-			encontrarNodo(v1).lista = aux; 
-		}else {
+			encontrarNodo(v1).lista = aux;
+		} else {
 			System.out.println("No existe alguino de los noods");
 		}
 	}
-	
 
-	
 	public void eliminarArista(int v1, int v2) {
 		NodoGrafo nodo = encontrarNodo(v1);
 		NodoArista arista = nodo.lista;
-		if(arista != null) {
-			if(arista.apunta.valor == v2) {				
+		if (arista != null) {
+			if (arista.apunta.valor == v2) {
 				nodo.lista = arista.sig;
-			}
-			else {
-				while(arista.sig != null && arista.apunta.valor == v2) {
+			} else {
+				while (arista.sig != null && arista.apunta.valor == v2) {
 					arista = arista.sig;
 				}
-				if(arista.sig != null) {
+				if (arista.sig != null) {
 					arista.sig = arista.sig.sig;
-				}				
-			}			
+				}
+			}
 		}
 	}
-	
-	
-
 
 	public boolean existeArista(int v1, int v2) {
 		NodoGrafo aux = this.encontrarNodo(v1);
-		NodoArista arista = aux.lista ;
-		while(arista != null) {
-			if(arista.apunta.valor ==v2) {
+		NodoArista arista = aux.lista;
+		while (arista != null) {
+			if (arista.apunta.valor == v2) {
 				return true;
 			}
 			arista = arista.sig;
-		}		
+		}
 		return false;
 	}
 
@@ -112,16 +111,15 @@ public class GrafoDinamic implements GrafosTDA {
 		return 0;
 	}
 
-
 	public void mostrarMatriz() {
 		NodoGrafo aux = nodo;
 		NodoArista arista;
-		while(aux != null) {
-		System.out.print(aux.valor + "\t" );
-		arista = aux.lista;
-			while(arista != null) {
-				
-				System.out.print(arista.peso + " "  + arista.apunta.valor +  "\t");
+		while (aux != null) {
+			System.out.print(aux.valor + "\t");
+			arista = aux.lista;
+			while (arista != null) {
+
+				System.out.print(arista.peso + " " + arista.apunta.valor + "\t");
 				arista = arista.sig;
 			}
 			System.out.println();
@@ -142,29 +140,46 @@ public class GrafoDinamic implements GrafosTDA {
 	}
 
 	public int[] conjuntoAislados() {
-		int aislados[]= new int [cantidad];
+		int aislados[] = new int[cantidad];
 		NodoGrafo aux = nodo;
-		
-		while(aux != null) {
-			
+
+		while (aux != null) {
+
 		}
 		return null;
 	}
 
 	public void imparAristas() {
 		// TODO Auto-generated method stub
-		
+
 	}
-	
+
 	public NodoGrafo encontrarNodo(int v) {
 		NodoGrafo aux = nodo;
-		while(aux != null) {
-			if(aux.valor == v) {
+		while (aux != null) {
+			if (aux.valor == v) {
 				return aux;
 			}
 			aux = aux.sig;
 		}
 		return null;
+	}
+
+	public Vector<NodoGrafo> Adyacentes(int v) {			//Nuevo Metodo
+		Vector<NodoGrafo> ady = new Vector<NodoGrafo>();
+		if (pertenece(v)) {
+			NodoGrafo origen = encontrarNodo(v);
+			NodoArista arista = origen.lista;
+			while (arista != null) {
+				NodoGrafo n = (NodoGrafo) arista.apunta;
+				ady.add(n);
+				arista = arista.sig;
+			}
+			return ady;
+		} else {
+			System.out.println("El nodo ingresado no pertence al grafo");
+			return null;
+		}
 	}
 
 	public int[] dephtFirstSearch() {
--- src/tp/DFS.java
+++ src/tp/DFS.java
@@ -1,5 +1,58 @@
 package tp;
 
+import java.util.Vector;
+
+import grafos.GrafoDinamic;
+import grafos.NodoGrafo;
+
 public class DFS {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		GrafoDinamic G = new GrafoDinamic ();
+		G.inicializarGrafo(4);
+		G.agregarVertice(1);
+		G.agregarVertice(2);
+		G.agregarVertice(3);
+		G.agregarVertice(4);
+		G.agregarVertice(6);
+		G.agregarVertice(7);
+		
+		G.agregarArista(1, 2, 4);
+		G.agregarArista(1, 4, 1);
+		G.agregarArista(2, 3, 2);
+		G.agregarArista(3, 4, 1);
+		G.agregarArista(6, 7, 2);
+		
+		
+		int origen = 3;
+		
+		DFS(G,6);
+		Marcado (G);
+	}
+	
+	static void DFS (GrafoDinamic x, int o) {
+		
+		if (x.pertenece(o)) {			// Validar que el numero que se ingresa como origen exista como Nodo del Grafo.
+			NodoGrafo origen = x.encontrarNodo(o);
+			origen.Visitado = true;
+			Vector<NodoGrafo> ady = x.Adyacentes(o);
+			for(NodoGrafo nodo: ady ) {
+					if (nodo.Visitado == false) {
+						o = nodo.valor;
+						DFS(x,o);
+					}
+			}
+			origen.marcado = true;
+		}
+	}
+	
+	static void Marcado (GrafoDinamic x){
+		for (int valorNodo: x.vertices()) {
+			NodoGrafo nodo = x.encontrarNodo(valorNodo);
+			System.out.println ("El nodo " + nodo.valor + " tiene el estado " + nodo.marcado);
+		}
+	}
 
 }
